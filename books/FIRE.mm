80e8a84d9ccbda21ff11fc75dacb4f5516b74647
Carlton Mason <ckmason@us.ibm.com>
2018-07-13 13:55:22 -0500

    initial version

---
+++ books/FIRE.mm
@@ -0,0 +1,230 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1531398682595" ID="ID_208750400" LINK="https://images-na.ssl-images-amazon.com/images/I/41n-n3XFlgL.jpg" MODIFIED="1531496679708">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      F.I.R.E by Dan Ward<br />
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1531398703960" HGAP="152" ID="ID_1832770222" MODIFIED="1531496714279" POSITION="left" TEXT="Key Take Aways" VSHIFT="-100">
+<node CREATED="1531428091695" HGAP="15" ID="ID_176431271" MODIFIED="1531429353666" TEXT="Placing a premium on speed, thrift, simplicity, and restraint, and then using those principles to shape our approach to problem solving and decision making.  " VSHIFT="-34">
+<icon BUILTIN="bookmark"/>
+</node>
+<node CREATED="1531399576586" HGAP="32" ID="ID_968984537" MODIFIED="1531429347739" TEXT="At its core, FIRE is about restraint. &#xa;- When in the name of restraint we build only the essential functions and include only the necessary interface components, we discover deep simplicity. Not simplicity for its own sake, but a simplicity that fosters quality, reliability, and usability.&#xa;- Similarly, when our commitment to restraint leads us to perform only the necessary activities, to only write useful documents and only hold meaningful meetings, we find that the pace of progress accelerates far beyond our hopes. Like simplicity, speed is a product of restraint. &#xa;- So is thrift&#x2014; doing less has a strong correlation with spending less in the long run." VSHIFT="-35"/>
+<node CREATED="1531398730156" HGAP="25" ID="ID_25391367" MODIFIED="1531422563708" VSHIFT="-7">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      FIRE is emphatically not a process improvement initiative - it is a VALUES system to help people make good decisions
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1531425709913" ID="ID_1664942247" MODIFIED="1531425746951" TEXT="For all the talk about speed and thrift, FIRE is not really about saving time and money as a primary goal.  &#xa;"/>
+<node CREATED="1531399715114" ID="ID_1276023743" MODIFIED="1531427407210" VSHIFT="16">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      The primary objective is to improve our objectives and outcomes rather than our processes or save money.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1531421217317" FOLDED="true" HGAP="23" ID="ID_1075790788" MODIFIED="1531492877471" POSITION="left" VSHIFT="-34">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      WTF?
+    </p>
+    <p>
+      <i>(What's The Fire?)</i>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1531420293351" HGAP="158" ID="ID_1166270327" MODIFIED="1531427741860" VSHIFT="-9">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>F&#160;is for Fast</b>&#160;- SPEED is GOOD, Slow kills.
+    </p>
+    <p>
+      - Agility beats strength every single time.<br />
+    </p>
+    <p>
+      - A tight schedule presents fewer opportunities for additional layers to accumulate and thus minimizes the snowballing
+    </p>
+    <p>
+      - Speed without quality is hasty, Speed != hasty.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <br />
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1531420763191" HGAP="164" ID="ID_498070220" MODIFIED="1531426899451" VSHIFT="30">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>I is for Inexpensive</b>
+    </p>
+    <p>
+      - Solving problems with intellectual capital instead of financial capital
+    </p>
+    <p>
+      - The more time and money we spend on the thing, the more complicated it gets, which in turn drives up the cost and schedule even further as we wrestle with all the complexity.
+    </p>
+    <p>
+      - Inexpensive != cheap
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1531421391795" HGAP="163" ID="ID_1270148279" MODIFIED="1531427045231" VSHIFT="48">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>R is for Restrained</b>
+    </p>
+    <p>
+      - It is a preference for self-control, for tight budgets and small teams, for short schedules, short meetings, and short documents.
+    </p>
+    <p>
+      - Constraints foster creativity
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1531421552946" HGAP="166" ID="ID_1028321052" MODIFIED="1531428805749" VSHIFT="40">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>E is for Elegant</b>
+    </p>
+    <p>
+      - Embracing elegant simplicity means designing our organizations and processes with simplicity in mind.
+    </p>
+    <p>
+      - It&#8217;s about stating our goals clearly and incorporating mature, proven technologies into our designs.
+    </p>
+    <p>
+      - True sophistication, true design maturity, and true process maturity are shown through deep simplicity, not through brain-meltingly complex diagrams and structures.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1531421764597" FOLDED="true" HGAP="30" ID="ID_386109590" MODIFIED="1531496704688" POSITION="right" TEXT="FIRE Principals" VSHIFT="-8">
+<node CREATED="1531399791697" HGAP="26" ID="ID_1450335879" MODIFIED="1531496699718" TEXT="YOU CANNOT DESIGN ANYTHING WITHOUT REVEALING YOUR VALUES " VSHIFT="58">
+<node CREATED="1531399876175" HGAP="36" ID="ID_1391876070" MODIFIED="1531490888716" VSHIFT="-12">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Project management, architecture, design, development are fundamentally a decision-making disciplines.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1531399922166" HGAP="34" ID="ID_350596107" MODIFIED="1531419722563" TEXT="The decisions we make shape the project&#x2019;s outcomes, but the decisions themselves&#x2014; particularly design decisions&#x2014; are shaped by the preferences, priorities, and values of the decision maker.  " VSHIFT="18"/>
+<node CREATED="1531422784574" HGAP="41" ID="ID_848759483" MODIFIED="1531490883812" TEXT="FIRE proposes a value set based on speed, thrift, simplicity, and self-control.   &#xa;&#xa;It posits that these four attributes are productive values and, when used to shape our decision making, lead to desirable outcomes" VSHIFT="24"/>
+</node>
+<node CREATED="1531421817608" FOLDED="true" HGAP="34" ID="ID_1230771218" MODIFIED="1531429382872" TEXT="YOU CAN&apos;T CHANGE JUST ONE THING" VSHIFT="62">
+<node CREATED="1531421904648" HGAP="58" ID="ID_810517586" MODIFIED="1531428987392" TEXT="Everything is connected to something, so in all but the simplest situations, changes to one aspect will have implications for other parts of the system. " VSHIFT="-63"/>
+</node>
+<node CREATED="1531422830258" FOLDED="true" ID="ID_1416755278" MODIFIED="1531493102471" TEXT="CONSTRAINTS FOSTER CREATIVITY">
+<node CREATED="1531422872617" HGAP="88" ID="ID_984445184" MODIFIED="1531428995312" TEXT="- Frugality thus serves as a forcing function to stimulate imaginative thinking. &#xa;- The result of this minimalist approach, if done well, is a distilled essence of what really matters.&#xa;- Establishing strict constraints therefore can help prevent overengineered solutions, bloated software, and incoherent PowerPoint decks.&#xa;" VSHIFT="-82"/>
+</node>
+<node CREATED="1531423139744" FOLDED="true" ID="ID_872112394" MODIFIED="1531493119239" TEXT="FOCUS FOSTERS SPEED">
+<node CREATED="1531423219971" HGAP="157" ID="ID_1210334090" MODIFIED="1531429228676" TEXT="- If your priorities are clear and your activities are oriented toward an overarching objective, you&#x2019;ll find that things move along with a minimal expenditure of time. &#xa;- Keeping the objective in sharp focus strips away distractions that slow us down. &#xa;- So one of the key FIRE disciplines is to establish and maintain a small list of Most Important Things. This list should not have more than three things on it. Any more than three, and we&#x2019;ll find ourselves bogged down, slowed down, and perpetually late to the party. " VSHIFT="-64"/>
+</node>
+<node CREATED="1531423024507" FOLDED="true" ID="ID_457102440" MODIFIED="1531491005040" TEXT="SPEED VALIDATES THE NEED">
+<node CREATED="1531423058800" HGAP="122" ID="ID_55698634" MODIFIED="1531429231728" TEXT="- A short-term due date indicates a greater degree of legitimacy than a long-term due date. &#xa;- When we need something right away, it&#x2019;s generally because we have a clear understanding of some capability gap." VSHIFT="11"/>
+</node>
+<node CREATED="1531423365650" FOLDED="true" ID="ID_270344675" MODIFIED="1531493148088" TEXT="TO FINISH EARLY START EARLY">
+<node CREATED="1531423423836" HGAP="98" ID="ID_314464347" MODIFIED="1531429005169" TEXT="- When we have a firm deadline, the only way to add more time to our schedule is on the front end.  &#xa;- The other way to start early is to leverage other people&#x2019;s work&#x2014; with proper attribution and remuneration, of course." VSHIFT="27"/>
+</node>
+<node CREATED="1531423597494" FOLDED="true" ID="ID_431049555" MODIFIED="1531429406869" TEXT="DELAY CAUSES DELAY">
+<node CREATED="1531423629209" HGAP="161" ID="ID_171921143" MODIFIED="1531429008355" TEXT="- A schedule delay increases the program&#x2019;s exposure to change, as new technologies, new threats, new people, and new economic realities conspire to place new demands on the project.  " VSHIFT="31"/>
+</node>
+<node CREATED="1531423704999" FOLDED="true" HGAP="34" ID="ID_1816574633" MODIFIED="1531429406869" TEXT="A PROJECT LEADER&#x2019;S INFLUENCE IS INVERSELY PROPORTIONAL TO THE BUDGET" VSHIFT="38">
+<node CREATED="1531423745739" HGAP="100" ID="ID_1815358827" MODIFIED="1531429035014" TEXT="- Smaller budgets correlate with autonomy &amp; influence.  &#xa;- Big budgets attract a lot of &quot;help&quot;" VSHIFT="12"/>
+</node>
+<node CREATED="1531423973558" FOLDED="true" HGAP="51" ID="ID_1109499896" MODIFIED="1531493220109" TEXT="COMPLEXITY IS NOT A SIGN OF SOPHISTICATION" VSHIFT="-5">
+<node CREATED="1531424086489" HGAP="74" ID="ID_1204271169" MODIFIED="1531429032111" TEXT="Simplicity is at the core of FIRE, and simplification generally involves reducing something to its core elements&#xa;- Making something more complicated requires effort, not skill; there is a difference&#xa;- Complexity is ultimately a sign of an immature design, not a sophisticated one.. &#xa;- Streamlining and trimming are what bring our design to maturity and ensure close alignment between what is built and what is needed.  &#xa;." VSHIFT="32"/>
+</node>
+<node CREATED="1531424400326" FOLDED="true" HGAP="55" ID="ID_86283045" MODIFIED="1531493245141" TEXT="A KICK-ASS HALF IS BETTER THAN A HALF-ASSED WHOLE  " VSHIFT="7">
+<node CREATED="1531424439690" ID="ID_151923241" MODIFIED="1531424491177" TEXT="- The idea is to be world-class at something rather than mediocre at everything. &#xa;- Pick the stuff that matters most and do it first.&#xa;"/>
+</node>
+<node CREATED="1531424527777" FOLDED="true" HGAP="49" ID="ID_1346541987" MODIFIED="1531493277699" TEXT="THE BEST WAY TO UNLEASH TALENT IS TO NOT HAVE TOO MUCH OF IT" VSHIFT="18">
+<node CREATED="1531424550985" ID="ID_572614741" MODIFIED="1531424660661" TEXT="- Small teams of talented people.&#xa;- Large teams tend to dilute or even mute the talents of the members,&#xa;- People are less proactive when placed in large groups -  social loafing...&#xa;"/>
+</node>
+<node CREATED="1531424693471" FOLDED="true" HGAP="47" ID="ID_150145039" MODIFIED="1531429406871" TEXT="MINIMZE THE DISTANCE BETWEEN DECISION AND ACTION" VSHIFT="10">
+<node CREATED="1531424763672" HGAP="49" ID="ID_1090265567" MODIFIED="1531429099264" TEXT="&#x201c;A decision should always be made at the lowest possible level and as close to the scene of action as possible.&#x201d;  " VSHIFT="14"/>
+</node>
+<node CREATED="1531424985167" FOLDED="true" HGAP="47" ID="ID_1375840942" MODIFIED="1531493326659" TEXT="THE TACTICAL ABILITY TO RAPIDLY DELIVER NEW CAPABILITIES IS ITSELF A STRATEGIC CAPABILITY  " VSHIFT="7">
+<node CREATED="1531425136870" HGAP="34" ID="ID_95630687" MODIFIED="1531429105206" TEXT="- &#x201c;build for the short term.&#x201d; - in an environment of rapid change, long-term projects are a losing proposition.  &#xa;- foster and maintain a persistent capacity to rapidly deal with the changing demands of the environment." VSHIFT="30"/>
+</node>
+<node CREATED="1531425240968" FOLDED="true" HGAP="43" ID="ID_224500067" MODIFIED="1531493345020" TEXT="THE FUTURE WILL BE SURPRIZING; PREPARE ACCORDINGLY" VSHIFT="3">
+<node CREATED="1531425278689" ID="ID_705238893" MODIFIED="1531425427560" TEXT="- important to build a responsive capacity for change into our systems. &#xa;- Modular designs, open architectures, well-defined interfaces, open standards"/>
+</node>
+<node CREATED="1531425444316" FOLDED="true" ID="ID_1315769368" MODIFIED="1531493379401">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      NO MORE THAN ONE MIRACLE&#160;PER PROJECT
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1531425494368" HGAP="66" ID="ID_393249517" MODIFIED="1531429122972" TEXT="- When a product design relies on a large number of unproven, immature technologies, the opportunity for significant cost overruns and schedule delays &#xa;- Therefore, product designs should take maximum advantage of existing materials, components, pieces, functions, and ideas. &#xa;- Simple, mature technology tends to reduce the uncertainty and instability inherent in cutting-edge, not-quite-proven technologies.&#xa;- We need to focus on building things we know how to build, using things we know how to use.&#xa;" VSHIFT="29"/>
+</node>
+</node>
+<node CREATED="1531426305528" FOLDED="true" ID="ID_1212043746" MODIFIED="1531493469902" POSITION="left" TEXT="FIRE Failure">
+<node CREATED="1531426465309" HGAP="186" ID="ID_1938664335" MODIFIED="1531429158069" TEXT="The myth of you get what you pay for &#xa;-  If we produce something that is fast, inexpensive, restrained, and elegant, we run the risk of being completely rejected by customers precisely because they would rather have a more expensive, complicated product. &#xa;- People love features. Features sell. In fact, consumer research shows that when given a choice between two similar products&#x2014; cameras, dishwashers, and the like&#x2014; shoppers generally prefer to pay a little extra for a product with more features. Further research shows that the vast majority of these extra features go completely unused.&#xa;&#xa;- It doesn&#x2019;t matter if they are making a good choice or not. It&#x2019;s their choice.  " VSHIFT="83"/>
+</node>
+</node>
+</map>
